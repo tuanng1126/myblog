17a276f524c743cfb3cfcf2ee2c5cad8ca8f9c7c
Wade Ng <wade.ng@vmodev.com>
2022-10-02 21:01:18 +0700

    Email support

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail
 
 from config import Config
 
@@ -16,6 +17,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+mail = Mail(app)
 
 if not app.debug:
     if not os.path.exists('logs'):
--- config.py
+++ config.py
@@ -7,4 +7,15 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'khong-doan-noi-dau'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     POSTS_PER_PAGE = 25
+
+    MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'smtp.gmail.com'
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 465)
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'tuanng.work@gmail.com'
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'evcslpcbajvquine'
+
+    ADMINS = ['tuanng.work@gmail.com']
+
