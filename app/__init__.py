207e65ab4f21c98835aeb4535d51d75d52f48c5e
Wade Ng <wade.ng@vmodev.com>
2022-10-02 21:16:57 +0700

    Date and time

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 from config import Config
 
@@ -20,6 +21,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 if not app.debug:
     if not os.path.exists('logs'):
